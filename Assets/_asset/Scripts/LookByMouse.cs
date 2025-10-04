41c93a8a3e6844ecbb2e677d6c7f1cd4f690a235
Minh <qminh2702@gmail.com>
2025-10-04 13:06:21 +0700

    dbf

--- Assets/_asset/Scripts/LookByMouse.cs
+++ Assets/_asset/Scripts/LookByMouse.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class LookByMouse : MonoBehaviour
 {
     public float anglePerSecond;
+     void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-    
     private void Update()
     {
-        float mouseX = Input.GetAxis("MouseX");
-        float mouseY = Input.GetAxis("MouseY");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
         float yaw = mouseX * anglePerSecond * Time.deltaTime;
-        transform.Rotate(0, yaw, 0);
+        float xaw = -mouseY * anglePerSecond * Time.deltaTime;
+        transform.Rotate(xaw, yaw, 0);
     }
 }
