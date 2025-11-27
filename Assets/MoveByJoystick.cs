e49493940e41de0ffedff241fe631decec9538a7
Minh <qminh2702@gmail.com>
2025-11-27 20:37:46 +0700

    mobile config

---
+++ Assets/MoveByJoystick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByJoystick : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float movingSpeed;
+    public Joystick joystick;
+
+    private void OnValidate()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float hInput = joystick.Horizontal;
+        float vInput = joystick.Vertical;
+        Vector3 direction = transform.right * hInput + transform.forward * vInput;
+        characterController.SimpleMove(direction * movingSpeed);
+        
+    }
+}
