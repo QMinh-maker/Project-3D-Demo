af48a7e94595e6bfea646ce6e47c743bd20a2ba5
Minh <qminh2702@gmail.com>
2025-12-01 23:45:51 +0700

    terrain

--- Assets/_asset/Scripts/ZombieSpawner.cs
+++ Assets/_asset/Scripts/ZombieSpawner.cs
@@ -12,35 +12,62 @@ public class ZombieSpawner : MonoBehaviour
     public float radius;
     public int spawnQuantity;
     public float spawnInterval;
-    public Transform PlayerFoot;
+    public Transform spawnPoint;
 
-    private bool hasStarted = false; //đánh dấu đã vào vùng
+    private bool isRunning;
+
+    //private bool hasStarted = false; //đánh dấu đã vào vùng
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, 0.2f);
+        if(spawnPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(spawnPoint.position, 0.2f);
+        }
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Handles.color = new Color(1, 0, 0, 0.1f);
-        Handles.DrawSolidDisc(transform.position, Vector3.up, radius);
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawSphere(transform.position, 0.2f);
+    //}
 #endif
 
-    //private void Start()
-    //{
-    //    StartCoroutine(SpawnZombieByTime());
-    //}
-    private void Update()
+    //    private void OnDrawGizmosSelected()
+    //    {
+    //        Handles.color = new Color(1, 0, 0, 0.1f);
+    //        Handles.DrawSolidDisc(transform.position, Vector3.up, radius);
+    //    }
+    //
+
+
+    //    private void Update()
+    //    {
+    //        // Nếu chưa từng chạy và player đã vào vùng radius
+    //        if (!hasStarted && Vector3.Distance(PlayerFoot.position, transform.position) <= radius)
+    //        {
+    //            hasStarted = true;
+    //            StartCoroutine(SpawnZombieByTime());
+    //        }
+    //    }
+
+    private void OnTriggerEnter(Collider other)
     {
-        // Nếu chưa từng chạy và player đã vào vùng radius
-        if (!hasStarted && Vector3.Distance(PlayerFoot.position, transform.position) <= radius)
+        if (!isRunning && other.CompareTag("Player"))
         {
-            hasStarted = true;
+            isRunning = true;
             StartCoroutine(SpawnZombieByTime());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isRunning && other.CompareTag("Player"))
+        {
+            isRunning = false;
+            StopAllCoroutines();
         }
     }
     private IEnumerator SpawnZombieByTime()
