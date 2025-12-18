c094f451e4654050885a93c56d4fbef874c10a5f
Minh <qminh2702@gmail.com>
2025-12-18 21:20:03 +0700

    cam setup

--- Assets/Helicopter/Scripts/UIGameController.cs
+++ Assets/Helicopter/Scripts/UIGameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIGameController : MonoBehaviour
 {
@@ -46,6 +47,6 @@ public class UIGameController : MonoBehaviour
 
     public void RestartGame()
     {
-        Application.LoadLevel("Main");
+        SceneManager.LoadScene("MainMenu");
     }
 }
